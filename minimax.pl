cf3257e0a91ba33e7b68c6a6bb92eb8ed6f5e9dd
Drette <mouffinette@hotmail.fr>
2014-06-15 17:00:48 +0200

    Update minimax.pl

--- minimax.pl
+++ minimax.pl
@@ -1,7 +1,12 @@
 :- module(minimax, [caseAJouer/3]).
 
-testFinal2(Hauteur,Result,Indice,Valeur):- simulationEnLargeur(1,0,JOrdi,JHumain,[3,3,0,1,3,1],[1,1,0,2,1,5],0,Feuilles),!,
+/* Premier agument hauteur, 2 eme argument case 0, troisieme argument joueur =1, 4 joueur ordi =0*/
+testFinal2(Indice,Valeur):- simulationEnLargeur(1,0,0,1,[3,3,0,1,3,1],[1,1,0,2,1,5],0,Feuilles),!,
 							maxDansListe(6,Feuilles,Indice,Valeur),!.
+
+testFinal3(Indice,Valeur):- simulationEnLargeur(1,0,1,0,[3,3,0,1,3,1],[1,1,0,2,1,5],0,Feuilles),!,
+							maxDansListe(6,Feuilles,Indice,Valeur),!.
+
 							
 simulationEnLargeur(Hauteur,6,_,_,_,_,_,[]):- !.
 simulationEnLargeur(0,_,_,_,_,_,_,[]):- !.
@@ -11,7 +16,20 @@ simulationEnLargeur(Hauteur,Case,J1,J2,P1,P2,MaxGrainesRamassees,[Feuilles|Feuil
 						simulationEnProfondeur(Hauteur,NouvelleCase,J1,J2,P1,P2,MaxGrainesRamassees,Feuilles),!.
 
 simulationEnProfondeur(0,_,_,_,_,_,Val,Val):- !. 
-simulationEnProfondeur(1,Case,J1,J2,P1,P2,MaxGrainesRamassees,GrainesRamassees):- tourPlateau(J1,J2,P1,P2,P1Fin,P2Fin,Case,GrainesRamassees),!.
+/*On passe en argument J1 =Joueir =1 J2=Joueur Ordi =0*/
+simulationEnProfondeur(1,Case,J1,J2,P3,P4,MaxGrainesRamassees,GrainesRamassees):- getPlateaux(J1, J2, OldP1, OldP2), getScore(J1, J2, SOld1, SOld2),
+																				tourPlateau(J1, J2, Case), 
+																				write('Scoreavanttour plateau \n'),
+																				write(SOld1),nl,write(SOld2),nl,
+																				write('Scoreaaprestour plateau\n '),
+																				getScore(J1, J2, SNew1, SNew2),
+																				write(SNew1),nl,write(SNew2),
+																				GrainesRamassees is (SNew1-SOld1),
+																				write('GrainesRamassees='),
+																				write(GrainesRamassees),
+																				majScoresSup(J1,J2,GrainesRamassees),
+																				majPlateaux(J1, J2, OldP1, OldP2),
+																				!.
 
 nouveauMax(Tete, ValMax, CaseMax, Case, Tete, Case):- Tete > ValMax; Tete == ValMax, Case \= 0,!.
 nouveauMax(Tete, ValMax, CaseMax, Case, ValMax, CaseMax):- !.
@@ -22,5 +40,7 @@ maxDansListe(NbCase,[T|Q],IndiceCase, Valeur):- NewNbCase is NbCase - 1,
 												maxDansListe(NewNbCase,Q,AncienIndiceCase,AncienMax),
 												CaseCourante is 7 - NbCase,
 												nouveauMax(T, AncienMax, AncienIndiceCase, CaseCourante, Valeur, IndiceCase).							 
-caseAJouer(ChoixCase,PlateauOrdi,PlateauJoueur):-simulationEnLargeur(1,0,PlateauOrdi,PlateauJoueur,0,Feuilles),!,
-							          maxDansListe(6,Feuilles,ChoixCase,Valeur),!.
+
+caseAJouer(ChoixCase,J1,J2):-getPlateaux(J1, J2, P1, P2),
+							simulationEnLargeur(1,0,J1,J2,P1,P2,0,Feuilles),!,
+							maxDansListe(6,Feuilles,ChoixCase,Valeur),!.
